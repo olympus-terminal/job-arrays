63def9da0dc48bfa92de757abe2938c9cd6a2be8
David Nelson <32707458+olympus-terminal@users.noreply.github.com>
2022-02-11 14:01:01 +0400

    Update prepare-slurm-filelist-example.sh

--- prepare-slurm-filelist-example.sh
+++ prepare-slurm-filelist-example.sh
@@ -3,4 +3,5 @@
 for f in *blastp.txt ; do echo "$f" >> blast-results.txt; done
 mkdir slurm-logs
 rm slurm-logs/*
+wc -l blast-results.txt
 echo "ready for extract_tally-contam_hits-accQ.sbatch deployment"
