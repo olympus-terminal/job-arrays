29b0cf09a34d43750c42ad1264127d9c2dbb3d87
David Nelson <32707458+olympus-terminal@users.noreply.github.com>
2022-02-16 16:30:02 +0400

    Update prepare-slurm-filelist-example.sh

--- prepare-slurm-filelist-example.sh
+++ prepare-slurm-filelist-example.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-for f in *blastp.txt ; do echo "$f" >> blast-results.txt; done
-mkdir slurm-logs
-rm slurm-logs/*
+if [[ -f blast-results.txt ]]
+then
+    rm blast-results.txt
+fi
+
+
+if [[ ! -f blast-results.txt ]]
+then
+    for f in *blastp.txt ; do echo "$f" >> blast-results.txt; done
+fi
+
+echo "There are"
 wc -l blast-results.txt
+echo "jobs to submit"
+
+
+if [[ -d slurm-logs ]]
+then
+    rm slurm-logs/*
+    echo "error logs and outputs will go to slurm-logs"
+fi
+
+if [[ ! -d slurm-logs ]]
+
+then
+   echo "error logs and outputs will go to slurm-logs"
+   mkdir slurm-logs
+fi
+
 echo "ready for extract_tally-contam_hits-accQ.sbatch deployment"
